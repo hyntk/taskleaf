ffd3c27abcc3ad53df1805596923dd53ccc1f07f
hyntk <hyntkjp@gmail.com>
2019-07-03 15:32:58 +0900

    feat: タスクの表示・加工対象を拡張

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,17 +8,34 @@ class TasksController < ApplicationController
   end
 
   def create
-    Task.create(task_params)
-    redirect_to new_task_path
+    @task=Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを作成しました！"
+    else
+      render 'new'
+    end
   end
 
   def show
     @task = Task.find(params[:id])
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを編集しました！"
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content,:status,:priority)
   end
 end
