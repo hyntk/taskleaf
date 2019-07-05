e26d55bd44f0296470bcdb59a6cae53d51f5476a
hyntk <hyntkjp@gmail.com>
2019-07-05 15:02:59 +0900

    feat:i18n化の実装

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module Taskleaf
                        request_specs: false
       g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
+
+    config.i18n.default_locale = :ja
   end
 end
