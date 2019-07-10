09b311aae04faafe85be552dbafaa97c9cb36ee9
hyntk <hyntkjp@gmail.com>
2019-07-10 18:20:22 +0900

    feat: カラムのdb制約の追加及びテストの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_014050) do
+ActiveRecord::Schema.define(version: 2019_07_10_082120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_07_10_014050) do
     t.string "priority", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "deadline"
+    t.date "deadline", null: false
   end
 
 end
