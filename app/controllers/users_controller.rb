e09281a2ade1c4753e93eaa370878b29949e54c1
hyntk <hyntkjp@gmail.com>
2019-07-18 18:57:21 +0900

    fix: usersコントローラのデバッグ構文削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      binding.pry
       redirect_to user_path(@user.id)
     else
       render 'new'
