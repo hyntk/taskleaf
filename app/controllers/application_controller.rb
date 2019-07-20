0d1afd3ab500a1e5cd3548f2733ae2ec8def102b
hyntk <hyntkjp@gmail.com>
2019-07-20 19:52:32 +0900

    fix: 画面遷移におけるユーザー認証についてテストを通らない問題を習性

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
 
   def ensure_correct_user
     @current_user = User.find_by(id:  session[:user_id])
-    if @current_user.id !=  params[:id].to_i
+    @task = Task.find_by(id:params[:id])
+    if @task.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+    # if @current_user.id !=  params[:id].to_i
+      binding.pry
       redirect_to tasks_path
     end
   end
