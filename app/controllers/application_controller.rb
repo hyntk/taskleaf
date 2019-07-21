4b971f47353734b8f68764d783f6720f677b54e6
hyntk <hyntkjp@gmail.com>
2019-07-21 13:51:53 +0900

    fix: showアクションでログインユーザに遷移する画面を許可するか判定するロジックを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,10 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   def ensure_correct_user
-    @current_user = User.find_by(id:  session[:user_id])
-    @task = Task.find_by(id:params[:id])
-    if @task.user_id != @current_user.id
-      flash[:notice] = "権限がありません"
+    @current_user = User.find_by(id: session[:user_id])
+    user = User.find(params[:id])#viewから送信されたパラメータを取得、Taskのアクションで使うとtaskのidが取得される
+    if user.id != @current_user.id
     # if @current_user.id !=  params[:id].to_i
-      binding.pry
       redirect_to tasks_path
     end
   end
