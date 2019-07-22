419b401743c80f6aec0a41b97b8cd738e2559c08
hyntk <hyntkjp@gmail.com>
2019-07-22 13:26:57 +0900

    feat: 管理画面からの新規ユーザ作成機能を作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,32 @@ class Admin::UsersController < ApplicationController
     @users = User.all.order("created_at DESC")
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path
+    else
+      render new_admin_user_path
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: t('view.task edited')
+    else
+      render 'admin_edit'
+    end
+  end
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
@@ -12,6 +38,11 @@ class Admin::UsersController < ApplicationController
   end
 
   private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 
   def authenticate_admin_user
     unless current_user.admin?
