82c6f50e72b8097e46fbf73f237b025115550102
hyntk <hyntkjp@gmail.com>
2019-07-25 16:52:14 +0900

    fix: アソシエーションの誤り修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,19 @@
 class User < ApplicationRecord
+  before_destroy :admin_at_least_one
+  # before_destroyコールバックは、dependent: :destroyよりも前に配置する
+  # 理由は、そのレコードがdependent: :destroyによって削除されるよりも前にbefore_destroyコールバックが実行されるようにするため
+
+  has_many :tasks,dependent: :destroy
+  # has_many :lavellings, dependent: :destroy
+
+  has_secure_password
+
+  validates :password, presence: true, length: { minimum: 6 }
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
-
-  before_destroy :admin_at_least_one
-  # before_destroyコールバックは、dependent: :destroyよりも前に配置する
-  # 理由は、そのレコードがdependent: :destroyによって削除されるよりも前にbefore_destroyコールバックが実行されるようにするため
-  has_many :tasks,dependent: :destroy
-  has_many :lavellings, dependent: :destroy
+  
     private
 
   def admin_at_least_one
